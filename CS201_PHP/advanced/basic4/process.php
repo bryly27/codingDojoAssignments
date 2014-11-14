d1e9e9624d60b8967c5e8ad4027f0a8f2cef6579
Bryant <bryly27@sbcglobal.net>
2014-11-13 22:29:54 -0800

    modified process

--- CS201_PHP/advanced/basic4/process.php
+++ CS201_PHP/advanced/basic4/process.php
@@ -6,13 +6,14 @@ if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
 		{
  	 	 	header('location: index.php');
  	 	 	$_SESSION['email']=$_POST['email'];
- 	 	 	$_SESSION['error']="<h3>The email address you entered (".$_SESSION['email'].") is NOT a valid email address!</h3></div>"
+ 	 	 	$_SESSION['error']="<h3>The email address you entered (".$_SESSION['email'].") is NOT a valid email address!</h3></div>";
  		}
  			else
  		{
  			header('location: success.php');
  			$_SESSION['email']=$_POST['email'];
  		}
+
 
 
 
